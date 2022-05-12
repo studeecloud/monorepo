7d7134f76df598bd2e3232b711b7b11e52b4bd97
Lisa Meyer <lisamermey@gmail.com>
2022-05-12 19:14:18 -0400

    Implement useState

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,16 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState();
+
   useEffect(() => {
     axios.get('http://localhost:3000/users')
     .then(res => {
       console.log(res.data);
+      setData(res.data)
+    })
+    .catch(err => { 
+      console.log(err) 
     })
   }, []);
 
