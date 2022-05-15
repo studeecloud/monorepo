22ff6e36895aa8439bfb2319281c5ca8db8c0711
Gonzo Nieto <5186441+gonzonieto@users.noreply.github.com>
2022-05-15 13:52:42 -0400

    add basic connection code and text box for unique token

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,10 @@ import { BigHead } from '@bigheads/core';
 import { connect, createLocalTracks } from 'twilio-video';
 
 function App() {
+  const queryParams = new URLSearchParams(window.location.search);
+  const token = queryParams.get('token');
+  console.log('LOGGING "token":', token);
+
   const [data, setData] = useState([]);
 
   useEffect(() => {
@@ -33,9 +37,54 @@ function App() {
     headArray.push(Example());
   }
 
+  createLocalTracks({
+    audio: true,
+    video: { width: 640 },
+  }).then((localTracks) => {
+    return connect(token, {
+      name: 'DailyStandup',
+      tracks: localTracks,
+    }).then(
+      (room) => {
+        console.log(`Successfully joined a Room: ${room}`);
+        room.on('participantConnected', (participant) => {
+          console.log(`A remote Participant connected: ${participant}`);
+
+          // Attach the Participant's Media to a <div> element
+          participant.tracks.forEach((publication) => {
+            if (publication.isSubscribed) {
+              const track = publication.track;
+              document
+                .getElementById('remote-media-div')
+                .appendChild(track.attach());
+            }
+          });
+
+          participant.on('trackSubscribed', (track) => {
+            document
+              .getElementById('remote-media-div')
+              .appendChild(track.attach());
+          });
+        });
+      },
+      (error) => {
+        console.error(`Unable to connect to Room: ${error.message}`);
+      }
+    );
+  });
+
   return (
     <main>
       <h1>StudeeCloud App</h1>
+
+      <h3>Token: {token}</h3>
+
+      <form action="" method="get">
+        <input type="text" name="token" style={{ border: '1px solid blue' }} />
+        <input type="submit" value="Submit" />
+      </form>
+
+      <div id="remote-media-div" style={{ border: '2px solid red' }}></div>
 
       {headArray}
     </main>
