c1e4d9995a876061bb90568029433f5c538a3df2
‘Gonzo <gonzonieto@gmail.com>
2022-05-15 20:02:11 +0000

    add unique key to Big Head array

--- client/src/App.js
+++ client/src/App.js
@@ -19,27 +19,23 @@ function App() {
       });
   }, []);
 
-  // Testing ability to generate Big Head
-  const Example = () => (
-    <div style={{ width: '4rem' }}>
-      <BigHead />
-    </div>
-  );
-
   const headNum = 5;
   const headArray = [];
 
-  for (let i = 0; i < headNum; i++) {
-    headArray.push(Example());
+  for (let i = 1; i <= headNum; i++) {
+    headArray.push(
+      <div style={{ width: '5rem' }} key={i.toString()}>
+        <BigHead />
+      </div>
+    );
   }
 
   return (
     <main style={{ margin: '0 0 0 1rem' }}>
       <h1>StudeeCloud App</h1>
 
-      <button class="btn btn-primary">Swag</button>
-
-      {headArray}
+      <button className="btn btn-primary">Swag</button>
+      <div>{headArray}</div>
     </main>
   );
 }
