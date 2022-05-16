d3bc2de9d6de319bc0ff1028d603aca0922ea4c2
Kehan Fu <kehanfu@gmail.com>
2022-05-16 04:39:21 +0000

    refactor timer into component with dynamic render of play/pause button depending on state

--- client/src/components/Timer.js
+++ client/src/components/Timer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import '../App.css';
 import { useTimer } from 'use-timer';
 
 
@@ -13,15 +13,24 @@ export default function Timer() {
       },
     });
 
-    console.log("this is the time ", time)
+    // console.log("this is the time ", time)
+    console.log("this is status", status)
     const min = Math.floor(time/60)
     const sec = (time - min * 60).toString().padStart(2, "0")
+
+    const startPauseButton = () => {
+      if(status === "RUNNING"){
+        return <button className="btn btn-primary" onClick={pause}>Pause</button>
+      } else {
+        return <button className="btn btn-primary" onClick={start}>Start</button>
+
+      }
+    }
 
   return(
     <div>
         <div>
-          <button className="btn btn-primary" onClick={start}>Start</button>
-          <button className="btn btn-primary" onClick={pause}>Pause</button>
+          {startPauseButton()}
           <button className="btn btn-primary" onClick={reset}>Reset</button>
         </div>
         <p>Elapsed time: {min}:{sec} </p>
