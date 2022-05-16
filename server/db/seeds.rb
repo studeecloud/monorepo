f7eedbe2d921bc77948583eb10df2e07a12dd45b
Lisa Meyer <36679211+l-m-meyer@users.noreply.github.com>
2022-05-15 23:53:11 -0400

    L wip/create express api (#1)
    Set up Express API server

--- server/db/seeds.rb
+++
@@ -1,5 +0,0 @@
-puts "Create users!"
-User.destroy_all
-user_one = User.create(email: 'v@k.ca', password: '123456')
-user_two = User.create(email: 'ab@c.ca', password: '123456')
-user_three = User.create(email: 'john@gmail.ca', password: '123456')
---
+++ server/db/seeds/01_users.sql
@@ -0,0 +1,3 @@
+INSERT INTO users  (first_name, last_name) VALUES ('Lisa', 'Meyer');
+INSERT INTO users  (first_name, last_name) VALUES ('Gonzo', 'Nieto');
+INSERT INTO users  (first_name, last_name) VALUES ('Kehan', 'Fu');
