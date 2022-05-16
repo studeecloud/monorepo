5cb034dc62cc63f520239757a80d7b62b01cebd8
Gonzo Nieto <5186441+gonzonieto@users.noreply.github.com>
2022-05-16 18:20:16 -0400

    add local video preview and event listeners for muted and disabled tracks

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,11 @@ import './App.css';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { BigHead } from '@bigheads/core';
-import { connect, createLocalTracks } from 'twilio-video';
+import {
+  connect,
+  createLocalTracks,
+  createLocalVideoTrack,
+} from 'twilio-video';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
@@ -12,6 +16,12 @@ import {
 } from '@fortawesome/fontawesome-svg-core/import.macro'; // <-- import styles to be used
 
 function App() {
+  // Display a local camera preview
+  createLocalVideoTrack().then((track) => {
+    const localMediaContainer = document.getElementById('local-media-div');
+    localMediaContainer.appendChild(track.attach());
+  });
+
   const queryParams = new URLSearchParams(window.location.search);
   const token = queryParams.get('token');
 
@@ -41,7 +51,6 @@ function App() {
   }
 
   let chatRoom = null;
-
   // Create the media tracks that this user will be broadcasting to the Room with the 'audio' and 'video' keys in the object that is sent to createLocalTracks()
   createLocalTracks({
     audio: true,
@@ -54,20 +63,54 @@ function App() {
     }).then(
       (room) => {
         // If we are in here, we successfully joined the room.
+
+        // If we receive an event indicating a track was disabled, execute the code inside
+        function handleTrackDisabled(track) {
+          track.on('disabled', () => {
+            console.log('Track disabled:');
+            console.log(track);
+          });
+        }
+        // If we receive an event indicating a track was enabled, execute the code inside
+        function handleTrackEnabled(track) {
+          track.on('enabled', () => {
+            console.log('Track enabled:');
+            console.log(track);
+          });
+        }
+
+        // Grab the room object so we can use it outside of this function
         chatRoom = room;
         console.log(`Successfully joined a Room: ${room}`);
 
-        // Attach the RemoteTracks of participants that are already in the room at the time that we join
         room.participants.forEach((participant) => {
-          // Iterate over the participant's media tracks, and if they are published, then attach them to the 'remote-media-div'
           participant.tracks.forEach((publication) => {
+            // Display the media tracks of participants that are already in the room
             if (publication.track) {
               document
                 .getElementById('remote-media-div')
                 .appendChild(publication.track.attach());
             }
-          });
+            // Attach the listeners to every subscribed media track
+            if (publication.isSubscribed) {
+              handleTrackEnabled(publication.track);
+              handleTrackDisabled(publication.track);
+            }
+            // When a new media track is subscribed, attach the listeners to it
+            publication.on('subscribed', handleTrackDisabled);
+            publication.on('subscribed', handleTrackEnabled);
 
+            publication.on('unsubscribed', () => {
+              console.log('Publication unsubscribed:');
+              console.log(publication);
+            });
+
+            publication.on('subscribed', () => {
+              console.log('Publication subscribed:');
+              console.log(publication);
+            });
+          });
+          // Display any new media tracks that are subscribed by participants in the room
           participant.on('trackSubscribed', (track) => {
             document
               .getElementById('remote-media-div')
@@ -139,28 +182,53 @@ function App() {
 
       <div
         id="remote-media-div"
-        style={{ border: '2px solid red', width: '12rem', height: '9rem' }}
+        style={{
+          display: 'inline-block',
+          border: '2px solid red',
+          width: '12rem',
+          height: '9rem',
+        }}
       ></div>
 
-      <button type="button" name="videoOff" onClick={() => muteVideo(chatRoom)}>
-        <FontAwesomeIcon icon={solid('video-slash')} />
-      </button>
+      <div
+        id="local-media-div"
+        style={{
+          display: 'inline-block',
+          border: '2px solid green',
+          width: '6rem',
+          height: '4.5rem',
+        }}
+      ></div>
 
-      <button
-        type="button"
-        name="videoOn"
-        onClick={() => enableVideo(chatRoom)}
-      >
-        <FontAwesomeIcon icon={solid('video')} />
-      </button>
+      <div>
+        <button
+          type="button"
+          name="videoOff"
+          onClick={() => muteVideo(chatRoom)}
+        >
+          <FontAwesomeIcon icon={solid('video-slash')} />
+        </button>
 
-      <button type="button" name="micOff" onClick={() => muteAudio(chatRoom)}>
-        <FontAwesomeIcon icon={solid('microphone-slash')} />
-      </button>
+        <button
+          type="button"
+          name="videoOn"
+          onClick={() => enableVideo(chatRoom)}
+        >
+          <FontAwesomeIcon icon={solid('video')} />
+        </button>
 
-      <button type="button" name="micOn" onClick={() => enableAudio(chatRoom)}>
-        <FontAwesomeIcon icon={solid('microphone')} />
-      </button>
+        <button type="button" name="micOff" onClick={() => muteAudio(chatRoom)}>
+          <FontAwesomeIcon icon={solid('microphone-slash')} />
+        </button>
+
+        <button
+          type="button"
+          name="micOn"
+          onClick={() => enableAudio(chatRoom)}
+        >
+          <FontAwesomeIcon icon={solid('microphone')} />
+        </button>
+      </div>
     </main>
   );
 }
