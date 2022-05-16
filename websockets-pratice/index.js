f124b8ee89a20663c1d2828e1e25e1e91de2049a
Kehan Fu <kehanfu@gmail.com>
2022-05-16 02:12:34 +0000

    pratice files for setting up synched timers

--- websockets-pratice/index.js
+++ websockets-pratice/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-
-console.log("this is http object", http)
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
+
+
 
 server.listen(3000, () => {
   console.log('listening on *:3000');
