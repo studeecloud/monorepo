e1bfe3f6ee8ea47f334b193a86180267a927005f
KehanYe <kehanfu@gmail.com>
2022-05-17 00:33:28 -0600

    implement slider framework and css

--- client/src/components/SettingButton.js
+++ client/src/components/SettingButton.js
@@ -1,5 +1,27 @@
-export default function SettingsButton(){
+import ReactSlider from 'react-slider';
+import './slider.css'
+
+export default function SettingsButton() {
   return(
-    <button className="btn btn-primary"> Setings </button>
+    <div style={{textAlign:'left'}}>
+      <label>work minutes: </label>
+      <ReactSlider
+        className={'slider'}
+        thumbClassName={'thumb'}
+        trackClassName={'track'}
+        value={45}
+        min={1}
+        max={120}
+      />
+      <label>break minutes: </label>
+      <ReactSlider
+        className={'slider'}
+        thumbClassName={'thumb'}
+        trackClassName={'track'}
+        value={45}
+        min={1}
+        max={120}
+      />
+    </div>
   )
 }
