49d7da50671103c43713c86f1ed33d14437cc313
Gonzo Nieto <5186441+gonzonieto@users.noreply.github.com>
2022-05-18 03:11:36 -0400

    add bg colour

--- client/src/components/TitlePanel.js
+++ client/src/components/TitlePanel.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 
-export default function TitlePanel({ title, bodyText, onSelect }) {
+export default function TitlePanel({ onSelect }) {
   // EXPECTED PROPS:
-  // title:String, bodyText:String, onSelect:Function
+  // onSelect:Function
 
   return (
     <section
-      className="dashboard__panel"
+      className="dashboard__panel bg-gold"
       style={{ border: '1px solid black' }}
       onClick={onSelect}
     >
