294c34ee2ecfb1dac849e9a29cead42c5ccec679
Lisa Meyer <lisamermey@gmail.com>
2022-05-17 23:28:58 -0400

    Update users schema according to ERD

--- server/db/schema/01_create_users.sql
+++ server/db/schema/01_create_users.sql
@@ -2,9 +2,9 @@ DROP TABLE IF EXISTS users CASCADE;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
-  first_name VARCHAR(255),
-  last_name VARCHAR(255)
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  username VARCHAR(255) NOT NULL,
+  avatar JSON,
+  settings JSON
 );
-
---avatar json, --check if this is the correct way to store the avatar/settings.
---settings json
