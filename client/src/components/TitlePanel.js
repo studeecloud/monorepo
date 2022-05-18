b50bf74f659473b5b9baf2ce0fbc1089c6e77f62
Gonzo Nieto <5186441+gonzonieto@users.noreply.github.com>
2022-05-18 02:35:34 -0400

    create Title Panel component

--- client/src/components/TitlePanel.js
+++ client/src/components/TitlePanel.js
@@ -10,10 +10,10 @@ export default function TitlePanel({ title, bodyText, onSelect }) {
       style={{ border: '1px solid black' }}
       onClick={onSelect}
     >
-      <h1 className="font-display text-5xl text-teal text-center">
+      <h1 className="font-display text-4xl text-black text-center">
         StudeeCloud
       </h1>
-      <h2 className="font-header text-4xl text-center">
+      <h2 className="font-header text-2xl text-center">
         Collaborative
         <br />
         Study Environment
