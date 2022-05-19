6803c0f354b1dfbbee06a129619bccddd1ae4630
Gonzo <5186441+gonzonieto@users.noreply.github.com>
2022-05-18 22:59:41 -0400

    Add descriptive comments to Pomodoro timer code (#9)
    * start websocket chat demo
    
    * implement test timer via user-timer package
    
    * basic timer with start, stop, reset
    
    * pratice files for setting up synched timers
    
    * inital steps to adding Timer component
    
    * refactor timer into component with dynamic render of play/pause button depending on state
    
    * testing timetoggle button
    
    * work implementing timer toggle button
    
    * rename exsisting timer as test template
    
    * add component framework for working timer
    
    * implement slider framework and css
    
    * create useContext hook for settings to manage timer setting state
    
    * implement SettingsContext with work and break minutes
    
    * implement back button to navigate from settings to timer
    
    * commit to handle merging main into local branch
    
    * Delete .npmrc
    
    * refactor and fix settings/back button
    
    * conditional rendering for play/pause buttons
    
    * implement timer logic and modeswitch between work and break
    
    * timer intialize with countdown, pause/start, switch between work/break
    
    * working timer, attempting to implement daisyui styling
    
    * prepping to merge from main
    
    * resolved merge issue and ran file
    
    * fix to display seconds
    
    * change progress bar colour depending on work/break mode
    
    * Delete .DS_Store
    
    * added comments to all code with FIXME, TODO, REVIEW
    
    * delete websockets practice folder
    
    Co-authored-by: Kehan Fu <kehanfu@gmail.com>
    Co-authored-by: kehan fu <93672764+KehanYe@users.noreply.github.com>

--- client/src/components/Timer.js
+++ client/src/components/Timer.js
@@ -1,27 +1,23 @@
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 
+//FIXME: Componenents have only a single onClick callback that changes isPaused state.
+// Can be removed added direclty to this document
 import PlayButton from './PlayButton';
 import PauseButton from './PauseButton';
 
 import { useContext, useState, useEffect } from 'react';
-import SettingsContext from './SettingsContext';
+import SettingsContext from './SettingsContext'; //FIXME: useContext component to be removed
 
 export default function Timer() {
-  const [isPaused, setIsPaused] = useState(false);
-  const [secondsLeft, setSecondsLeft] = useState(0);
-  const [mode, setMode] = useState('work');
+  const [isPaused, setIsPaused] = useState(false); //Used by pause & play buttons on timer
+  const [mode, setMode] = useState('work'); // "Work" and "Play" alternate once timer reaches 0
+  const [secondsLeft, setSecondsLeft] = useState(0); //Each mode has independent secondsLeft state
 
+  // FIXME: Refactor using just props (see values being passed through Provide component in PomodoroTimer file)
   const settingsInfo = useContext(SettingsContext);
 
-  // console.log('this is mode', mode);
-  // console.log('this is isPaused', isPaused);
-  // console.log('this is secondsLeft', secondsLeft);
-  // console.log('this is settingsInfo', settingsInfo);
-
-  function tick() {
-    setSecondsLeft((prev) => prev - 1);
-  }
-
+  // Initializes timer with "work" mode first
+  // FIXME: All settingsInfo will be replaced props
   useEffect(() => {
     function initTimer() {
       setSecondsLeft(settingsInfo.workMinutes * 60);
@@ -38,12 +34,15 @@ export default function Timer() {
           ? settingsInfo.workMinutes * 60
           : settingsInfo.breakMinutes * 60
       );
-      // const nextSeconds =
-      //   (nextMode === "work"
-      //     ? settingsInfo.workMinutes
-      //     : settingsInfo.breakMinutes) * 60;
     }
 
+    // Decrement timer helper function
+    function tick() {
+      setSecondsLeft((prev) => prev - 1);
+    }
+
+    //Main function controlling timer countdown for both mode
+    //TODO: Rename as potential timerCountDown?
     const interval = setInterval(() => {
       if (isPaused) {
         return;
@@ -64,15 +63,21 @@ export default function Timer() {
     settingsInfo.breakMinutes,
   ]);
 
+  // Helper functions to calcuate time left in Min:Sec format
   const totalSeconds =
     mode === 'work'
       ? settingsInfo.workMinutes * 60
       : settingsInfo.breakMinutes * 60;
-  const percentage = Math.round((secondsLeft / totalSeconds) * 100);
 
   const minutes = Math.floor(secondsLeft / 60);
   let seconds = secondsLeft % 60;
+  // Conditional needed to display leading 0
   if (seconds < 10) seconds = '0' + seconds;
+
+  // Helper function to calcuate perfentage of time left
+  // REVIEW: Helper function is used in CircularProgressBar Componenet.
+  // Not MVP nor does it necessarily follow our theme. Also, DaisyUI has progress bar components
+  const percentage = Math.round((secondsLeft / totalSeconds) * 100);
 
   return (
     <div>
