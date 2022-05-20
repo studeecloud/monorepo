7642f1d1a28e675f6fcd1c6535739e675f3eed92
KehanYe <kehanfu@gmail.com>
2022-05-20 17:19:49 -0600

    implement static-file uploading middleware with express server and sync with react front howler component

--- server/app.js
+++ server/app.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const {ENVIRONMENT, PORT} = process.env;
+const { ENVIRONMENT, PORT } = process.env;
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
@@ -9,7 +9,7 @@ const bodyParser = require('body-parser');
 const db = require('./configs/db.config');
 
 // routes import
-const usersRoutes = require('./routes/users'); 
+const usersRoutes = require('./routes/users');
 
 const app = express();
 
@@ -18,11 +18,20 @@ app.use(cors());
 app.use(morgan(ENVIRONMENT));
 app.use(bodyParser.json());
 
+// app.use(express.static('sounds'));
+
+//Sound Library -> format is /sounds/[filename.mp3]
+app.use('/sounds', express.static(__dirname + '/sounds'));
+
+console.log(__dirname);
+
 // routes
 app.use('/users', usersRoutes(db));
 
 app.get('/', (req, res) => {
-  res.json({greetings: 'hello world'});
+  res.json({ greetings: 'hello world' });
 });
+
+// app.get('/sounds/JaydaG.mp3', (req, res) => {});
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
