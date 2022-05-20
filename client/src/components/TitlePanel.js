d02393ffae5b8c38ba49d90293e49f1320eaf8d3
Lisa Meyer <36679211+l-m-meyer@users.noreply.github.com>
2022-05-20 19:07:21 -0400

    Add button to expand each panel (#15)

--- client/src/components/TitlePanel.js
+++ client/src/components/TitlePanel.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 import PomodoroTimer from './PomodoroTimer';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  solid,
+  regular,
+  brands,
+} from '@fortawesome/fontawesome-svg-core/import.macro';
 
 export default function TitlePanel({ onSelect }) {
   // EXPECTED PROPS:
@@ -9,8 +15,15 @@ export default function TitlePanel({ onSelect }) {
     <section
       className="dashboard__panel bg-gold"
       style={{ border: '1px solid black' }}
-      onClick={onSelect}
+    // onClick={onSelect}
     >
+      <button
+        type='button'
+        className="flex mx-6"
+        onClick={onSelect}
+      >
+        <FontAwesomeIcon icon={solid('expand')} className="h-7" />
+      </button>
       <h1 className="font-display text-4xl text-black text-center">
         StudeeCloud
       </h1>
