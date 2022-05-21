565b80832aadc81de5e390240c1cccbc08d7c112
KehanYe <kehanfu@gmail.com>
2022-05-20 23:53:30 -0600

    start impelmenting sound level bar

--- client/src/components/DiscoSound.js
+++ client/src/components/DiscoSound.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Howl } from 'howler';
+import ReactSlider from 'react-slider';
 
 export default function DiscoSound() {
   const sound = new Howl({
@@ -13,7 +14,24 @@ export default function DiscoSound() {
   return (
     <div>
       <div onClick={() => sound.play()}> Play Disco</div>
+      <ReactSlider
+        className={'slider'}
+        thumbClassName={'thumb'}
+        trackClassName={'track'}
+        min={1}
+        max={60}
+        onChange={(newValue) => (sound.volume = newValue)}
+      />
       <div onClick={() => sound.pause()}> Pause Disco</div>
     </div>
   );
 }
+
+// display: inline-block;
+// border-radius: 3px;
+// padding: 0.5rem 0;
+// margin: 0.5rem 1rem;
+// width: 11rem;
+// background: transparent;
+// color: white;
+// border: 2px solid white;
