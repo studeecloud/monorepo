1e8f800f0cd0ffab55aa414ef8ab067f94893f7f
Lisa Meyer <lisamermey@gmail.com>
2022-05-21 10:14:39 -0400

    Add logic to render messages

--- client/src/components/MessageList.js
+++ client/src/components/MessageList.js
@@ -1,6 +1,22 @@
 import Message from "./Message";
 
 export default function MessageList(props) {
+
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
+  const renderMessages = (messages) => {
+    for (let message of messages) {
+      <Message
+        user={message.user}
+        body={message.body}
+      />
+    }
+  }
+
   return (
     <Message />
   );
