60b36606a250d3ad6d17137506d1d67e3d0fce72
Gonzo Nieto <5186441+gonzonieto@users.noreply.github.com>
2022-05-21 19:01:43 -0400

    manually set message IDs based on count+1

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -2,21 +2,29 @@ const router = require('express').Router();
 
 module.exports = (db) => {
   router.get('/', (req, res) => {
-    const getMessages = "SELECT * FROM messages";
-    db.query(getMessages)
-      .then(data => {
-        console.log('message data', data);
-        res.json(data.rows);
-      })
-
+    const getMessages = 'SELECT * FROM messages';
+    db.query(getMessages).then((data) => {
+      console.log('message data', data);
+      res.json(data.rows);
+    });
   });
   router.post('/', (req, res) => {
-    const insertMessage = `INSERT INTO messages VALUES($1)`;
+    const insertMessage = `INSERT INTO messages VALUES($1, $2)`;
+    const msgCountQuery = `SELECT COUNT(*) FROM messages;`;
     console.log('req', req.body);
-    db.query(insertMessage, [req.body.message_text])
-      .then(data => {
-        res.json({ "status": "success" });
-      })
+
+    db.query(msgCountQuery).then((data) => {
+      console.log(
+        'LOGGING "parseInt(data.rows[0].count)":',
+        parseInt(data.rows[0].count)
+      );
+      const msgCount = parseInt(data.rows[0].count);
+      db.query(insertMessage, [msgCount + 1, req.body.message_text]).then(
+        (data) => {
+          res.json({ status: 'success' });
+        }
+      );
+    });
   });
   return router;
-}
+};
