518ce88c25787bd7878e9e66e0378b728c3b3852
Lisa Meyer <lisamermey@gmail.com>
2022-05-21 14:58:23 -0400

    Refactor to display all messages

--- client/src/components/MessageList.js
+++ client/src/components/MessageList.js
@@ -2,22 +2,19 @@ import Message from "./Message";
 
 export default function MessageList(props) {
 
-  const escape = function (str) {
-    let div = document.createElement("div");
-    div.appendChild(document.createTextNode(str));
-    return div.innerHTML;
-  };
-
-  const renderMessages = (messages) => {
-    for (let message of messages) {
-      <Message
-        user={message.user}
-        body={message.body}
-      />
-    }
-  }
-
   return (
-    <Message />
-  );
-};
+    <>
+      {
+        props.messages.map(message => {
+          return (
+            <Message
+              user='LISA'
+              body={message.message_text}
+            />
+          )
+        }
+        )
+      }
+    </>
+  )
+}
