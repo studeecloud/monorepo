29740dd49104987a139be3b544e6e32b46234c19
KehanYe <kehanfu@gmail.com>
2022-05-21 10:46:10 -0600

    import useState

--- client/src/components/MessageForm.js
+++ client/src/components/MessageForm.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 
 export default function MessageForm(props) {
   const [message, setMessage] = useState();
@@ -5,14 +6,30 @@ export default function MessageForm(props) {
   const placeholder = 'Talk to me...';
   const disableSubmit = (e) => {
     e.preventDefault();
-  }
+  };
 
   return (
     <section>
-      <form method="post" action="/messages" className='flex flex-col items-center' onSubmit={disableSubmit}>
-        <textarea type='text' className='border-2 border-dark-gray p-2 rounded' name='text' placeholder={placeholder} style={{ width: 500, height: 75 }}></textarea>
-        <input type='submit' value='Message' className='border-2 border-dark-gray p-2 rounded w-48 my-2.5' onClick={disableSubmit} />
+      <form
+        method="post"
+        action="/messages"
+        className="flex flex-col items-center"
+        onSubmit={disableSubmit}
+      >
+        <textarea
+          type="text"
+          className="border-2 border-dark-gray p-2 rounded"
+          name="text"
+          placeholder={placeholder}
+          style={{ width: 500, height: 75 }}
+        ></textarea>
+        <input
+          type="submit"
+          value="Message"
+          className="border-2 border-dark-gray p-2 rounded w-48 my-2.5"
+          onClick={disableSubmit}
+        />
       </form>
     </section>
   );
-};
+}
