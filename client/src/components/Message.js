d786805b8c135ce127784d880070bd33b78ce431
Lisa Meyer <lisamermey@gmail.com>
2022-05-21 10:13:46 -0400

    Adjust styling to fit theme

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -7,7 +7,7 @@ export default function Message(props) {
   }
 
   return (
-    <article className="flex border-2 rounded p-2 bg-gold h-30 my-2.5" style={{ width: 500 }}>
+    <article className="flex border-2 border-dark-gray rounded p-2 bg-gold h-30 my-2.5" style={{ width: 500 }}>
       <header>
         {/* <BigHead className="h-20 w-20" /> */}
         <h2 className="m-1 font-body"><strong>{message.user}</strong></h2>
