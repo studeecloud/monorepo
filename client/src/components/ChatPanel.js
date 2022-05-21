16a14713aa84899d9e31b8629216b0cfa5062ad6
Lisa Meyer <lisamermey@gmail.com>
2022-05-20 20:53:51 -0400

    Format Chat Panel, import MessageForm/ MessageList

---
+++ client/src/components/ChatPanel.js
@@ -0,0 +1,26 @@
+import MessageForm from "./MessageForm";
+import MessageList from "./MessageList";
+import { BigHead } from '@bigheads/core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
+
+export default function ChatPanel({ onSelect }) {
+  return (
+    <section
+      className="dashboard__panel bg-meringue"
+      style={{ border: '1px solid black' }}
+    >
+      <button
+        type='button'
+        className="flex mx-6"
+        onClick={onSelect}
+      >
+        <FontAwesomeIcon icon={solid('expand')} className="h-7" />
+      </button>
+      <h1 className="font-display text-4xl text-black text-center">Chat</h1>
+      <MessageList />
+      <MessageForm />
+    </section>
+
+  );
+};
