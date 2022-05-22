2bf6f48a869643a4f56f6f6bfbf94e296b6cfbd7
Gonzo <5186441+gonzonieto@users.noreply.github.com>
2022-05-22 01:49:47 -0400

    Move submit button beside textarea element (#43)
    * refactor placeholder and disable resizing of textarea element
    
    * set a default username to catch cases where username is null
    
    * add bottom margin to h1 and format w Prettier
    
    * set to full width of parent
    
    * set to width of parent
    
    * format w Prettier

--- client/src/components/MessageList.js
+++ client/src/components/MessageList.js
@@ -1,27 +1,22 @@
-import Message from "./Message";
-
+import Message from './Message';
 
 /**
- * 
- * @param {Object} messages includes two keys: {String} message and {String} sender 
+ *
+ * @param {Object} messages includes two keys: {String} message and {String} sender
  * @returns All Message components
  */
 export default function MessageList({ messages }) {
-
   return (
     <>
-      {
-        messages.map((message, i) => {
-          return (
-            <Message
-              key={i}
-              user={message.sender.toUpperCase()}
-              body={message.message}
-            />
-          )
-        }
-        )
-      }
+      {messages.map((message, i) => {
+        return (
+          <Message
+            key={i}
+            user={message.sender.toUpperCase()}
+            body={message.message}
+          />
+        );
+      })}
     </>
-  )
+  );
 }
