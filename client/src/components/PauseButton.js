6d9745da940449423e2de93e3d050b7a7e81a517
Gonzo Nieto <5186441+gonzonieto@users.noreply.github.com>
2022-05-22 03:06:57 -0400

    style timer play/pause buttons

--- client/src/components/PauseButton.js
+++ client/src/components/PauseButton.js
@@ -1,13 +1,13 @@
 export default function PauseButton({ setPause }) {
   return (
     <button
-      className="btn btn-primary"
+      className="bg-meringue border-2 border-dark-purple font-body p-2 rounded text-dark-gray text-2xl"
       onClick={() => {
         setPause();
       }}
     >
-      {" "}
-      Pause{" "}
+      {' '}
+      Pause{' '}
     </button>
   );
 }
--- client/src/components/PlayButton.js
+++ client/src/components/PlayButton.js
@@ -1,8 +1,11 @@
-export default function PlayButton({setPlay}) {
+export default function PlayButton({ setPlay }) {
   return (
-    <button className="btn btn-primary" onClick={() => setPlay()}>
-      {" "}
-      Play{" "}
+    <button
+      className="bg-meringue border-2 border-dark-purple font-body p-2 rounded text-dark-gray text-2xl"
+      onClick={() => setPlay()}
+    >
+      {' '}
+      Play{' '}
     </button>
   );
 }
