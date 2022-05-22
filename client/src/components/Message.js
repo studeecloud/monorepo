cb82d52d232f0d69cb4aa32e7d28842f8b3b5702
Lisa Meyer <lisamermey@gmail.com>
2022-05-22 15:16:26 -0400

    Change styling

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -7,15 +7,15 @@ export default function Message(props) {
   // }
 
   return (
-    <article className="flex border-2 border-dark-gray rounded p-2 bg-gold h-30 w-full mb-2">
+    <article className="flex border-2 border-dark-gray rounded p-2 bg-meringue h-30 w-full mb-2">
       <header>
         {/* <BigHead className="h-20 w-20" /> */}
-        <h2 className="m-1 font-body">
+        <h2 className="m-1 font-header w-24">
           <strong>{props.user}</strong>
         </h2>
       </header>
 
-      <p className="m-auto font-body">{props.body}</p>
+      <p className="m-auto ml-3 font-body">{props.body}</p>
     </article>
   );
 }
