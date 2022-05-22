2c4f62f73f3f46c7e28c6cd433dca48cafad2ae6
Gonzo <5186441+gonzonieto@users.noreply.github.com>
2022-05-22 02:44:34 -0400

    Set focus buttons to absolute position and move to top-right corner of each panel (#47)
    * set focus buttons to absolute and removed horizontal margin
    
    * move focus panel buttons to top-right corner of each panel

--- client/src/components/SoundPanel.js
+++ client/src/components/SoundPanel.js
@@ -12,14 +12,19 @@ import Ghibli from './Ghibli';
 export default function TitlePanel({ onSelect }) {
   return (
     <section
-      className="dashboard__panel bg-meringue"
+      className="dashboard__panel relative bg-meringue"
       style={{ border: '1px solid black' }}
     >
-      <button type="button" className="flex mx-6" onClick={onSelect}>
+      <button
+        type="button"
+        className="mt-3 absolute"
+        style={{ width: '93%' }}
+        onClick={onSelect}
+      >
         <FontAwesomeIcon icon={solid('expand')} className="h-7" />
       </button>
 
-      <h1 className="font-display text-4xl text-black text-center">
+      <h1 className="mt-3 mb-5 font-display text-4xl text-black text-center">
         Soundboard
       </h1>
       <h2 className="font-body text-lg text-center">SFX</h2>
