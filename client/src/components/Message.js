501199893d2a774ad4e984239b8bdd0da725719c
Gonzo Nieto <5186441+gonzonieto@users.noreply.github.com>
2022-05-22 01:14:05 -0400

    set to full width of parent

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -1,20 +1,21 @@
-import { BigHead } from "@bigheads/core";
+import { BigHead } from '@bigheads/core';
 
 export default function Message(props) {
-
   // const message = {
   //   user: 'LISA',
   //   body: 'Isn\'t StudeeCloud the best?!'
   // }
 
   return (
-    <article className="flex border-2 border-dark-gray rounded p-2 bg-gold h-30 my-2.5" style={{ width: 500 }}>
+    <article className="flex border-2 border-dark-gray rounded p-2 bg-gold h-30 w-full mb-2">
       <header>
         {/* <BigHead className="h-20 w-20" /> */}
-        <h2 className="m-1 font-body"><strong>{props.user}</strong></h2>
+        <h2 className="m-1 font-body">
+          <strong>{props.user}</strong>
+        </h2>
       </header>
 
-      <p className='m-auto font-body'>{props.body}</p>
+      <p className="m-auto font-body">{props.body}</p>
     </article>
   );
-};
+}
