b7f8fa4f6b68eaabd5030d69eed1945b216a8bee
Lisa Meyer <lisamermey@gmail.com>
2022-05-23 17:17:47 -0400

    update messages schema

--- server/db/schema/04_create_messages.sql
+++ server/db/schema/04_create_messages.sql
@@ -3,5 +3,6 @@ DROP TABLE IF EXISTS messages CASCADE;
 CREATE TABLE messages (
   id SERIAL PRIMARY KEY,
   message TEXT,
-  sender VARCHAR(255)
+  sender VARCHAR(255),
+  room_name VARCHAR(255)
 );
