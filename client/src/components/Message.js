108a7388daba2ce62a6f46d3751d4e54d2a5dc83
Lisa Meyer <lisamermey@gmail.com>
2022-05-24 17:17:14 -0400

    Style enhancements

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -1,13 +1,13 @@
-export default function Message(props) {
+export default function Message({ user, body }) {
   return (
-    <article className="flex border border-dark-gray rounded p-2 bg-meringue mb-3">
+    <article className="flex flex-col border border-dark-gray rounded px-2 bg-meringue mb-3">
       <header>
-        <h2 className="m-1 font-header w-24 break-all">
-          <strong>{props.user}</strong>
+        <h2 className="mx-1 font-body break-all">
+          <small><strong>{user}</strong></small>
         </h2>
       </header>
 
-      <p className="my-auto ml-3 font-body break-all">{props.body}</p>
+      <p className="my-auto mx-1 font-body break-all">{body}</p>
     </article>
   );
 }
