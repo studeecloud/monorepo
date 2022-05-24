ed7597f7c3890355c26427cf07088d1b480e22a4
Lisa Meyer <lisamermey@gmail.com>
2022-05-24 14:38:52 -0400

    Style improvements

--- client/src/components/TitlePanel.js
+++ client/src/components/TitlePanel.js
@@ -12,35 +12,37 @@ export default function TitlePanel(props) {
   } = props;
 
   return (
-    <section className="dashboard__panel relative border bg-meringue py-4">
+    <section className="dashboard__panel relative border bg-teal py-4">
       <div className="h-full flex flex-col justify-end">
         <div className="my-auto">
-          <h1 className="mt-3 mb-5 font-display text-5xl text-center">
+          <h1 className="mt-3 mb-5 font-display text-meringue text-5xl text-center">
             StudeeCloud
           </h1>
 
-          <h2 className="font-header text-3xl text-center">
-            Collaborative
-            <br />
-            Study Environment
+          <h2 className="font-header text-meringue text-3xl text-center">
+            <strong>
+              Collaborative
+              <br />
+              Study Environment
+            </strong>
           </h2>
         </div>
         <div className="flex items-center justify-center">
-          <h3 className="font-body text-2xl">
-            <strong>Room: </strong>
+          <h3 className="font-body text-meringue text-2xl">
+            <strong className="font-header text-gold">Room: </strong>
             {roomName}
           </h3>
 
           <button
             type="button"
-            className="text-body text-xl border p-2 m-3 rounded"
+            className="text-body text-gold text-xl border p-2 m-3 rounded"
             onClick={() => navigator.clipboard.writeText(joinRoomLink)}
           >
             <FontAwesomeIcon icon={light('clone')} />
           </button>
         </div>
 
-        <h3 className="font-body text-2xl text-center">
+        <h3 className="font-body text-2xl text-meringue text-center m-auto">
           Share your room link with your team!
         </h3>
       </div>
