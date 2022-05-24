5042a34d1de5a891cf076f07702ffbf2854b4a12
Lisa Meyer <lisamermey@gmail.com>
2022-05-24 17:04:53 -0400

    Remove unused import and commented out code

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -1,21 +1,13 @@
-import { BigHead } from '@bigheads/core';
-
 export default function Message(props) {
-  // const message = {
-  //   user: 'LISA',
-  //   body: 'Isn\'t StudeeCloud the best?!'
-  // }
-
   return (
     <article className="flex border border-dark-gray rounded p-2 bg-meringue mb-3">
       <header>
-        {/* <BigHead className="h-20 w-20" /> */}
-        <h2 className="m-1 font-header w-24">
+        <h2 className="m-1 font-header w-24 break-all">
           <strong>{props.user}</strong>
         </h2>
       </header>
 
-      <p className="my-auto ml-3 font-body break-all">{props.body}</p>
+      <p className="my-auto ml-3 font-body">{props.body}</p>
     </article>
   );
 }
